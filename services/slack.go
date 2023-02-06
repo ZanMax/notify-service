1ec9166ab80eae75d4767ca5cdf1b25036efb517
maxim <zanoga.maxim@gmail.com>
2023-02-06 13:49:00 -0800

    add slack integration

--- services/slack.go
+++ services/slack.go
@@ -1,1 +1,12 @@
 package services
+
+import "github.com/slack-go/slack"
+
+func sendMessageToSlack(text string, channelID string, token string) (bool, error) {
+	slackClient := slack.New(token)
+	_, _, err := slackClient.PostMessage(channelID, slack.MsgOptionText(text, false))
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
